def3402773ee22dbceb543701a0534ecffc1479d
eladtzorani <tzorani.elad@gmail.com>
2023-05-05 14:41:11 +0300

    arguments passed through function

--- circle_drawer.py
+++ circle_drawer.py
@@ -4,10 +4,19 @@ matplotlib.rcParams['figure.figsize'] = [10, 10]
 import matplotlib.pyplot as plt
 plt.switch_backend('QT4Agg')
 import math, cmath
-from matplotlib.animation import FuncAnimation, PillowWriter
+from matplotlib.animation import FuncAnimation
 import numpy as np
+from functools import partial
 
-margin = 0.1
+speed = 1 
+increment = 1/100
+
+ax=plt.axes()
+ax.set_facecolor(background_color)
+index = count()
+x_vals = []
+y_vals = []
+trace = [[],[]]
 
 def order_coefficients(coefficients):
     return dict(sorted(coefficients.items(), key=lambda item: np.abs(item[1]), reverse=True))
@@ -18,40 +27,11 @@ def determine_size(coefficients):
         radius_sum = radius_sum + np.abs(coefficients[i])
     return 2 * (radius_sum + margin)
 
-arrow_color = '#ffffff'
-trace_color = '#ffdd00'
-background_color = '#000033'
-circle_color = arrow_color
-
-speed = 1 
-increment = 1/100
-
-coefficients = {
-        0:0,
-        -1:1,
-        5:0.5,
-        10:0.7
-        }
-coefficients = order_coefficients(coefficients)
-
-canvas_size = determine_size(coefficients)
-#coefficients = [complex(0), complex(1), complex(0), complex(0), complex(0), complex(0.5), complex(0), complex(0), complex(0), complex(0), complex(0.7)]
-
-ax=plt.axes()
-ax.set_facecolor(background_color)
-index = count()
-x_vals = []
-y_vals = []
-trace = [[],[]]
-
-
-coefficients = order_coefficients(coefficients)
-
 
 def plot_graph(coefficients):
     positions, angles = zip(*[cmath.polar(z) for z in coefficients])
 
-def update_points():
+def update_points(coefficients):
     t = next(index)
     
     x = [coefficients[0].real]
@@ -65,7 +45,7 @@ def update_points():
     trace[1].append(y[-1])
     return x, y
 
-def draw_arrows(x,y):
+def draw_arrows(x,y,arrow_color,trace_color,background_color):
     for i in range(1, len(x)):
         dx = x[i] - x[i-1]
         dy = y[i] - y[i-1]
@@ -74,11 +54,11 @@ def draw_arrows(x,y):
         circ = plt.Circle((x[i-1], y[i-1]), radius, fill=False, color=circle_color, linewidth = radius * 0.5)
         plt.gca().add_patch(circ)
 
-def animate(i):
+def animate(i, coefficients, canvas_size, margin, arrow_color):
     plt.cla()
 
     for i in range(speed):
-        x_vals, y_vals = update_points()
+        x_vals, y_vals = update_points(coefficients)
     
     plt.cla()
     draw_arrows(x_vals, y_vals)
@@ -87,8 +67,16 @@ def animate(i):
     plt.ylim(-canvas_size/2, canvas_size/2)
     plt.autoscale(False)
 
-ani = FuncAnimation(plt.gcf(), animate, interval=1)
-plt.tight_layout()
-plt.axis('scaled')
+def circle_drawing(coefficients, ordering = True, margin = 0.1, arrow_color = '#ffffff', trace_color = '#ffdd00', background_color = '#000033', circle_color = None):
+    canvas_size = determine_size(coefficients)
+    if(ordering == True):
+        coefficients = order_coefficients(coefficients)
+    if circle_color is None:
+        circle_color = arrow_color
+    ani = FuncAnimation(plt.gcf(), partial(animate, coefficients = coefficients, canvas_size = canvas_size, margin = margin,
+                                           arrow_color = arrow_color, trace_color = trace_color, background_color = background_color,
+                                           circle_color = circle_color), interval=1)
+    plt.tight_layout()
+    plt.axis('scaled')
 
-plt.show()
+    plt.show()
